a21d5acadfc0c2b4fe03b23d8ebcc3266bc2c599
jayrutger <jar110@students.uwf.edu>
2020-03-21 01:33:58 -0500

    Main getting better, brute force pretty much done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,91 @@
 #include "AdjacencyMatrix.hpp"
 
+bool valid = false;
+
+int numCities, numTours, numGenerations, numPercent;
+
 int main()
 {
+
+	do{
+		valid = true;
+		std::cout << "Please enter number of cities to run: " << std::endl;
+		
+		std::cin >> numCities;
+
+		if(std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter only integers\n";
+			valid=false;
+
+		}
+
+	}while(valid==false);
+
+	
+	
+	do{
+		valid = true;
+		
+		std::cout << "How many individual tours are in a given generation: " << std::endl;
+		
+		std::cin >> numTours;
+
+		if(std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter only integers\n";
+			valid=false;
+
+		}
+
+	}while(valid==false);
+
+
+
+	do{
+		valid = true;
+		
+		std::cout << "Please enter how many generations to run: " << std::endl;
+		
+		std::cin >> numGenerations;
+
+		if(std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter only integers\n";
+			valid=false;
+
+		}
+
+	}while(valid==false);
+
+
+	
+	do{
+		valid = true;
+		
+		std::cout << "Please enter what percentage of generation should be compromised of mutations: " << std::endl;
+		
+		std::cin >> numPercent;
+
+		if(std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter only integers\n";
+			valid=false;
+
+		}
+
+	}while(valid==false);
+
+
+	
 
 	AdjacencyMatrix obj;
 
@@ -9,7 +93,7 @@ int main()
 
 //	obj.PrintMatrix();
 		
-	obj.Permutate(12);	
+	obj.Permutate(11);	
 
 //	obj.GetDistance();
 	
